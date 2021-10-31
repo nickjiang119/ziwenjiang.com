d1c39fc47f30ade3c3ed3211f234443d0c5f10ef
nickjiang119 <nickjiang119@hotmail.com>
2021-10-31 19:53:33 -0400

    Add files via upload
    initial commit

---
+++ PHPMailer/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+echo ("hello world");
+// require_once('src/PHPMailer.php');
+
+// $mail = new PHPMailer();
+// $mail->isSMTP();
+// $mail->SMTPAuth() = true;
+// $mail->SMTPSecure = 'ssl';
+// $mail->Host = 'smtp.gmail.com';
+// $mail->Port = '465';
+// $mail->isHTML();
+// $mail->Username = 'dongjiang19@gmail.com';
+// $mail->Password = 'Jh690116';
+// $mail->SetFrom = 'no-reply@dongjiang19@gmail.com';
+// $mail->Subject = 'Hello World';
+// $mail->Body = 'test email';
+// $mail->AddAddress('nickjiang119@hotmail.com');
+
+// $mail->Send();
+?>
